7e4fbf10b27db38027238a94ceea419317bc5464
Luiz Araujo <arauldesign@gmail.com>
2018-03-15 13:10:01 -0300

    Maiusculas e Minusculas

---
+++ strings/exemplo02.php
@@ -0,0 +1,19 @@
+<?php 
+
+$nome = "Luiz Araujo";
+
+echo $nome;
+echo "<br>";
+
+//texto todo maiusculo
+echo strtoupper($nome);
+//texto todo minusculo
+echo "<br>";
+echo strtolower($nome);
+//as primeiras letras das palavras em maiuscula
+echo "<br>";
+echo ucwords($nome);
+//a primeira letra da plavra em maiuscula
+echo "<br>";
+echo ucfirst($nome);
+ ?>
